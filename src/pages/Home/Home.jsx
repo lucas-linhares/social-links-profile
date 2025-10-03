0d83a8317eb6ee2b752413ff55b448c3446d2603
Lucas Linhares <lucaslinhares_@outlook.com>
2025-10-03 01:02:29 -0300

    feat: Adiciona o componente ProfileCard a página Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import ProfileCard from "../../components/ProfileCard/ProfileCard"
+
 export default function Home() {
     return (
         <div>
             <main>
-                
+                <ProfileCard />
             </main>
         </div>
     )
