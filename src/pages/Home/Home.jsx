f91c90b42d6d97f15d43aa46411ae6d5beb60ef0
Lucas Linhares <lucaslinhares_@outlook.com>
2025-10-02 23:54:56 -0300

    Modifica a estrutura html da página Home
    A estrutura semântica do HTML estavana na página Home, realoquei a estrtura para dentros dos componentes.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,20 +2,7 @@ export default function Home() {
     return (
         <div>
             <main>
-                <section>
-                    <header>
-                        <h1>Jessica Randall</h1>
-                        <p>London, United Kingdom</p>
-                    </header>
-
-                    <p>Front-end developer and avid reader.</p>
-
-                    <nav>
-                        <ul>
-                            
-                        </ul>
-                    </nav>
-                </section>
+                
             </main>
         </div>
     )
