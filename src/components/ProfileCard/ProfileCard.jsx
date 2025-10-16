d4ce044f421fa847e838801496fe6317e1ef155a
Lucas Linhares <lucaslinhares_@outlook.com>
2025-10-16 00:19:55 -0300

    style: adiciona a imagem do perfil para mobile e desktop e melhora o espaçamento dos elemento
    Foi adicionada duas tags, picture e source, para melhorar responsividade das imagens em mobile e desktop. Também melhorei os espaçamento entre alguns elementos.

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,10 +1,9 @@
 import styles from './ProfileCard.module.css'
-import avatar from '../../assets/images/avatar-jessica.jpeg'
 import SocialLink from '../SocialLink/SocialLink.jsx'
+import avatarMobile from '../../assets/images/avatar-jessica-mobile.jpg'
+import avatarDesktop from '../../assets/images/avatar-jessica-desktop.jpeg'
 
 function ProfileCard() {
-
-    const profileAvatar = avatar
 
     return (
         <div>
@@ -12,9 +11,11 @@ function ProfileCard() {
                 <div className={styles.wrapper}>
                     <section className={styles.sec}>
                         <header className={styles.header}>
-                            <figure>
-                                <img src={profileAvatar} alt='Avatar-profile' className={styles.header__avatar}/>
-                            </figure>
+                            <picture>
+                                <source srcSet={avatarMobile} media="(max-width: 600px)"/>
+                                <source srcSet={avatarDesktop} media="(min-width: 601px)"/>
+                                <img src={avatarMobile} alt='Avatar profile' className={styles.header__avatar} />
+                            </picture>
                             <div className={styles.header__content}>
                                 <h1 className={styles.header__mainTitle}>Jessica Randall</h1>
                                 <p className={styles.header__paragraph}>London, United Kingdom</p>
